e5818842a7df0a397d490caa234e22b6d28ad09f
abdou <a.khebchi@esi-sba.dz>
2023-09-29 16:40:09 +0000

    adding the agent logic

---
+++ metriko-agent/src/agent.rs
@@ -0,0 +1,9 @@
+use std::io::Write;
+
+pub fn send_data_to_server(collectet_data : metriko_common::MetrikoApi) 
+{
+    let bytes_to_send = metriko_common::encode(collectet_data);
+    println!("encoding {} bytes of data ",bytes_to_send.len());
+    let mut tcp_stream = std::net::TcpStream::connect(metriko_common::METRIKO_PORT).unwrap();
+    tcp_stream.write_all(&bytes_to_send).unwrap()
+}
--- metriko-agent/src/main.rs
+++ metriko-agent/src/main.rs
@@ -1,3 +1,15 @@
+mod agent;
+mod puller;
+
 fn main() {
-    println!("Metriko agent : Hello, world!");
+    println!("starting metrico-agent");
+    let (tx , rx) = std::sync::mpsc::
+    channel::<metriko_common::MetrikoApi>();
+    let _puller_thread = std::thread::spawn(move || {
+        puller::pull_metrics_from_machine(tx);
+    });
+    while  let Ok(collected_data) = rx.recv() {
+            println!("sending updated metrics to server");
+            agent::send_data_to_server(collected_data);
+    }
 }
